752c50b32cabc948d487be194174ff6471115c9d
Brian Lee <briandl92391@gmail.com>
2022-12-21 09:50:05 -0800

    format

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,12 +1,12 @@
-import {useContext} from "react";
-import {ReturnNavigationContext} from "./context";
+import { useContext } from 'react';
+import { ReturnNavigationContext } from './context';
 
 export function useReturnLocation() {
-  const {returnLocation} = useContext(ReturnNavigationContext);
+  const { returnLocation } = useContext(ReturnNavigationContext);
   return returnLocation;
 }
 
 export function useReturnNavigationOptions() {
-  const {defaultReturnLocationParam} = useContext(ReturnNavigationContext);
-  return {defaultReturnLocationParam};
+  const { defaultReturnLocationParam } = useContext(ReturnNavigationContext);
+  return { defaultReturnLocationParam };
 }
